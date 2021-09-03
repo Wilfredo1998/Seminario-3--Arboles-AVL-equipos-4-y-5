a0f33cd633f4e3c370a8fb1fd4e2ad786e4b2c8c
rondon20193 <90071460+rondon20193@users.noreply.github.com>
2021-09-04 01:29:17 +0200

    Add files via upload

---
+++ Parte de Ranfis equipo 5.cpp
@@ -0,0 +1,21 @@
+// funcion para recorrido en profumdidad = preorden
+void preorden (nodo *arbol){
+	return
+}
+else {
+	count << arbol ->datos<< " - ";
+	preorden(arbol = >izq);
+	preorden(arbol = >der);
+}
+//funcione spara recorrido en profundidad 
+
+void i norden(nodo*arbol){
+	if ( arbol = = NULL){
+	return 	
+}
+else {
+	i norden ( arbol -> izq);
+	i norden ( arbol -> dato << " - ";
+	i norden ( arbol -> der );
+	
+}
