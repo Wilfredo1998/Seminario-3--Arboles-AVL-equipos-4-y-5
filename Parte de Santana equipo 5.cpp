215aaa66d00068de9789a67c69db8ecba7b6e7b8
soldier3023 <90014639+soldier3023@users.noreply.github.com>
2021-09-03 09:31:14 +0800

    Add files via upload

---
+++ Parte de Santana equipo 5.cpp
@@ -0,0 +1,34 @@
+Swich (opcion)  {
+Case 1:
+	Cout<< "digite un numero:  ":
+	Cin>> dato:
+	insertarNodo(arbol, dato, NULL):
+	cout << "/n":
+	system("pause");
+	break:
+Case 2:
+	Cout<< "aquí tyienes el árbol impreso de forma equilibrada:  ":
+	Mostrararbol (árbol, contador): 
+	cout << "/n":
+	system("pause");
+	break:
+Case 3:
+	Cout<< "/n" <<introduce el dato aue estas buscando  ":
+	Cin>> dato:
+	If (búsqueda ( árbol, dato) == true) {
+		cout << "/n" << "el elemto" << dato<< "si se encuentra en el árbol" << end{:
+}
+Else {
+cout << "/n" << "el elemto" << dato<< "si no se encuentra en el árbol" << end{:
+}
+	system("pause");
+	break:
+
+Case 4:
+	Cout<< "/nrecorrido en preorden:" << end: 
+	Preorden(arbol)
+	cout << "/n":
+	system("pause");
+	break:
+
+
